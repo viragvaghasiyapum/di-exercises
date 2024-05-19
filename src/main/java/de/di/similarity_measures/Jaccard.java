c0c5ea899b245bfa0b283a5e9a43abc924851148
viragvaghasiyapmu <vaghasiy@students.uni-marburg.de>
2024-05-19 22:49:10 +0200

    Implemented Jaccard Solution

--- src/main/java/de/di/similarity_measures/Jaccard.java
+++ src/main/java/de/di/similarity_measures/Jaccard.java
@@ -2,6 +2,8 @@ package de.di.similarity_measures;
 
 import de.di.similarity_measures.helper.Tokenizer;
 import lombok.AllArgsConstructor;
+import org.apache.commons.text.similarity.IntersectionResult;
+import org.apache.commons.text.similarity.JaccardSimilarity;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -31,6 +33,7 @@ public class Jaccard implements SimilarityMeasure {
 
         String[] strings1 = this.tokenizer.tokenize(string1);
         String[] strings2 = this.tokenizer.tokenize(string2);
+
         return this.calculate(strings1, strings2);
     }
 
@@ -44,20 +47,62 @@ public class Jaccard implements SimilarityMeasure {
      */
     @Override
     public double calculate(String[] strings1, String[] strings2) {
-        double jaccardSimilarity = 0;
-
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         //                                      DATA INTEGRATION ASSIGNMENT                                           //
         // Calculate the Jaccard similarity of the two String arrays. Note that the Jaccard similarity needs to be    //
         // calculated differently depending on the token semantics: set semantics remove duplicates while bag         //
         // semantics consider them during the calculation. The solution should be able to calculate the Jaccard       //
         // similarity either of the two semantics by respecting the inner bagSemantics flag.                          //
-
-
-
         //                                                                                                            //
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+        //  Note : bag semantics - don't remove duplicates from neither the union nor intersection
+        //         set semantics - remove duplicates from both union and intersection
+
+        double jaccardSimilarity = 0;
+        Arrays.sort(strings1);
+        Arrays.sort(strings2);
+
+        if (bagSemantics) {
+            Map<String, Integer> countElem1 = new HashMap<>();
+            Map<String, Integer> countElem2 = new HashMap<>();
+            ArrayList<String> intersection = new ArrayList<>();
+
+            // counting occurrences of each element in strings1 array
+            for (String token : strings1) {
+                countElem1.put(token, countElem1.containsKey(token) ? countElem1.get(token) + 1 : 1);
+            }
+            // counting occurrences of each element in strings2 array
+            for (String token : strings2) {
+                countElem2.put(token, countElem2.containsKey(token) ? countElem2.get(token) + 1 : 1);
+            }
+            // creating intersection array with duplicate elements
+            for (Map.Entry<String, Integer> entry : countElem1.entrySet()) {
+                String key = entry.getKey();
+                if (!countElem2.containsKey(key)) {
+                    continue;
+                }
+                int occurrence = Math.min(entry.getValue(), countElem2.get(key));
+                while(occurrence > 0) {
+                    intersection.add(key);
+                    --occurrence;
+                }
+            }
+            jaccardSimilarity = (double) intersection.size() / (strings1.length + strings2.length);
+        } else {
+            Set<String> set1 = new HashSet<>(Arrays.asList(strings1));
+            Set<String> set2 = new HashSet<>(Arrays.asList(strings2));
+
+            // intersection without duplicates
+            Set<String> intersection = new HashSet<>(set1);
+            intersection.retainAll(set2);
+
+            // union of unique elements
+            Set<String> union = new HashSet<>(set1);
+            union.addAll(set2);
+
+            jaccardSimilarity = (double) intersection.size() / union.size();
+        }
         return jaccardSimilarity;
     }
 }
