af3080cff721335d8c013d142cc76e576f908ae1
viragvaghasiyapmu <vaghasiy@students.uni-marburg.de>
2024-06-09 00:38:22 +0200

    di-virag: updated IND profiler file.

--- src/main/java/de/di/data_profiling/INDProfiler.java
+++ src/main/java/de/di/data_profiling/INDProfiler.java
@@ -1,8 +1,14 @@
 package de.di.data_profiling;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 import de.di.Relation;
 import de.di.data_profiling.structures.IND;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Array;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -14,24 +20,69 @@ public class INDProfiler {
      * @return The list of all non-trivial unary (and n-ary) inclusion dependencies in the provided relations.
      */
     public List<IND> profile(List<Relation> relations, boolean discoverNary) {
+
+        if (discoverNary) {
+             throw new RuntimeException("nary discovery is not implemented!");
+        }
+
+        Map<String, List<String[]>> allRelations = new HashMap<>();
+        for (Relation relation : relations) {
+            allRelations.put(relation.getName(), readCsvData(relation));
+        }
+
         List<IND> inclusionDependencies = new ArrayList<>();
-
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //                                      DATA INTEGRATION ASSIGNMENT                                           //
-        // Discover all inclusion dependencies and return them in inclusion dependencies list. The boolean flag       //
-        // discoverNary indicates, whether only unary or both unary and n-ary INDs should be discovered. To solve     //
-        // this assignment, only unary INDs need to be discovered. Discovering also n-ary INDs is optional.           //
-
-
-
-        //                                                                                                            //
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-        if (discoverNary)
-            // Here, the lattice search would start if n-ary IND discovery would be supported.
-            throw new RuntimeException("Sorry, n-ary IND discovery is not supported by this solution.");
-
+        for (Relation table1 : relations) {
+            for (Relation table2 : relations) {
+                if (!table1.getName().equals(table2.getName())) {
+                    discoverUnaryInclusionDependencies(table1, table2, allRelations, inclusionDependencies);
+                    System.out.println(inclusionDependencies.size());
+                }
+            }
+        }
         return inclusionDependencies;
+    }
+
+    private List<String[]> readCsvData(Relation relation) {
+
+        List<String[]> data = new ArrayList<>();
+        try (CSVReader csvReader = new CSVReader(new FileReader(System.getProperty("user.dir")+"/data/data_profiling/"+relation.getName()+".csv"))) {
+            String[] values;
+            while ((values = csvReader.readNext()) != null) {
+                values = Arrays.asList(values).get(0).replaceAll("\"", "").split(";");
+                values = Arrays.stream(values).map(String::trim).toArray(String[]::new);
+                data.add(values);
+            }
+        } catch (IOException | CsvValidationException e) {
+            throw new RuntimeException(e);
+        }
+        return data;
+    }
+
+    private void discoverUnaryInclusionDependencies (Relation r1, Relation r2, Map<String, List<String[]>> relations, List<IND> inclusionDependencies) {
+        for (String attr1 : r1.getAttributes()) {
+            Set<String> attr1Values = getColumnValues(relations.get(r1.getName()), attr1);
+
+            for (String attr2 : r2.getAttributes()) {
+                Set<String> attr2Values = getColumnValues(relations.get(r2.getName()), attr2);
+
+                if (attr2Values.containsAll(attr1Values)) {
+                    inclusionDependencies.add(new IND(r1, 1, r2, 1));
+                }
+            }
+        }
+    }
+
+    private Set<String> getColumnValues(List<String[]> tableData, String columnName) {
+
+        Set<String> columnValues = new HashSet<>();
+        int columnIndex = Arrays.asList(tableData.get(0)).indexOf(columnName);
+        if (columnIndex < 0) {
+            throw new IllegalArgumentException("Column " + columnName + " does not exist in table data.");
+        }
+        for (int row = 1; row < tableData.size(); row++) {
+            columnValues.add(Arrays.asList(tableData.get(row)).get(columnIndex));
+        }
+        return columnValues;
     }
 
     private List<Set<String>> toColumnSets(String[][] columns) {
