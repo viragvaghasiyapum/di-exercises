f21ffbd8b2a5162d860ea450cbfd6e301d63fc93
viragvaghasiyapmu <vaghasiy@students.uni-marburg.de>
2024-06-09 12:03:49 +0200

    di-virag: optimized IND profiler file.

--- src/main/java/de/di/data_profiling/INDProfiler.java
+++ src/main/java/de/di/data_profiling/INDProfiler.java
@@ -3,11 +3,9 @@ package de.di.data_profiling;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
 import de.di.Relation;
+import de.di.data_profiling.structures.AttributeList;
 import de.di.data_profiling.structures.IND;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.sql.Array;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -25,69 +23,53 @@ public class INDProfiler {
              throw new RuntimeException("nary discovery is not implemented!");
         }
 
-        Map<String, List<String[]>> allRelations = new HashMap<>();
-        for (Relation relation : relations) {
-            allRelations.put(relation.getName(), readCsvData(relation));
-        }
-
         List<IND> inclusionDependencies = new ArrayList<>();
         for (Relation table1 : relations) {
             for (Relation table2 : relations) {
                 if (!table1.getName().equals(table2.getName())) {
-                    discoverUnaryInclusionDependencies(table1, table2, allRelations, inclusionDependencies);
-                    System.out.println(inclusionDependencies.size());
+                    discoverUnaryInclusionDependencies(table1, table2, inclusionDependencies);
                 }
             }
         }
+        System.out.println("Non-Trivial Unary Inclusion Dependencies ::");
         return inclusionDependencies;
     }
 
-    private List<String[]> readCsvData(Relation relation) {
+    private void discoverUnaryInclusionDependencies (Relation r1, Relation r2, List<IND> inclusionDependencies) {
 
-        List<String[]> data = new ArrayList<>();
-        try (CSVReader csvReader = new CSVReader(new FileReader(System.getProperty("user.dir")+"/data/data_profiling/"+relation.getName()+".csv"))) {
-            String[] values;
-            while ((values = csvReader.readNext()) != null) {
-                values = Arrays.asList(values).get(0).replaceAll("\"", "").split(";");
-                values = Arrays.stream(values).map(String::trim).toArray(String[]::new);
-                data.add(values);
-            }
-        } catch (IOException | CsvValidationException e) {
-            throw new RuntimeException(e);
-        }
-        return data;
-    }
+        List<String> r1Attributes = Arrays.asList(r1.getAttributes());
+        List<String> r2Attributes = Arrays.asList(r2.getAttributes());
 
-    private void discoverUnaryInclusionDependencies (Relation r1, Relation r2, Map<String, List<String[]>> relations, List<IND> inclusionDependencies) {
-        for (String attr1 : r1.getAttributes()) {
-            Set<String> attr1Values = getColumnValues(relations.get(r1.getName()), attr1);
-
-            for (String attr2 : r2.getAttributes()) {
-                Set<String> attr2Values = getColumnValues(relations.get(r2.getName()), attr2);
-
+        for (String attr1 : r1Attributes) {
+            Set<String> attr1Values = getColumnValues(r1, attr1);
+            for (String attr2 : r2Attributes) {
+                Set<String> attr2Values = getColumnValues(r2, attr2);
                 if (attr2Values.containsAll(attr1Values)) {
-                    inclusionDependencies.add(new IND(r1, 1, r2, 1));
+                    inclusionDependencies.add(new IND(r1, r1Attributes.indexOf(attr1), r2, r2Attributes.indexOf(attr2)));
                 }
             }
         }
     }
 
-    private Set<String> getColumnValues(List<String[]> tableData, String columnName) {
-
+    private Set<String> getColumnValues(Relation r, String columnName) {
         Set<String> columnValues = new HashSet<>();
-        int columnIndex = Arrays.asList(tableData.get(0)).indexOf(columnName);
+        String[] attributes = r.getAttributes();
+
+        int columnIndex = Arrays.asList(attributes).indexOf(columnName);
         if (columnIndex < 0) {
             throw new IllegalArgumentException("Column " + columnName + " does not exist in table data.");
         }
-        for (int row = 1; row < tableData.size(); row++) {
-            columnValues.add(Arrays.asList(tableData.get(row)).get(columnIndex));
+        int rSize = r.getRecords().length;
+        String[][] records = r.getRecords();
+        for (int row = 0; row < rSize; row++) {
+            columnValues.add(records[row][columnIndex].trim());
         }
         return columnValues;
     }
 
-    private List<Set<String>> toColumnSets(String[][] columns) {
-        return Arrays.stream(columns)
-                .map(column -> new HashSet<>(new ArrayList<>(List.of(column))))
-                .collect(Collectors.toList());
-    }
+//    private List<Set<String>> toColumnSets(String[][] columns) {
+//        return Arrays.stream(columns)
+//                .map(column -> new HashSet<>(new ArrayList<>(List.of(column))))
+//                .collect(Collectors.toList());
+//    }
 }
